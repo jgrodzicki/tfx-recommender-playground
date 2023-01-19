63b399b7c3c418b57f43d7ecb628e72222bb6ccc
Jakub  Grodzicki <jakub.grodzicki@forecast.global>
2023-01-19 16:03:41 +0100

    Add method for creating `SchemaGen` to `PipelineFactory`

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,3 +1,4 @@
+from tfx.components.schema_gen.component import SchemaGen
 from tfx.components.statistics_gen.component import StatisticsGen
 
 from src.components.custom_example_gen import CustomExampleGen
@@ -11,3 +12,7 @@ class PipelineFactory:
     @staticmethod
     def _create_statistics_gen(example_gen: CustomExampleGen) -> StatisticsGen:
         return StatisticsGen(examples=example_gen.outputs["examples"])
+
+    @staticmethod
+    def _create_schema_gen(statistics_gen: StatisticsGen) -> SchemaGen:
+        return SchemaGen(statistics=statistics_gen.outputs["statistics"])
