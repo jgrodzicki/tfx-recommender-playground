112f6b9d6ed8f3e14b42ba2edb2e35117fdf44a1
Jakub  Grodzicki <jakub.grodzicki@forecast.global>
2023-01-19 16:01:43 +0100

    Add method for creating `StatisticsGen` to `PipelineFactory`

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,3 +1,5 @@
+from tfx.components.statistics_gen.component import StatisticsGen
+
 from src.components.custom_example_gen import CustomExampleGen
 
 
@@ -5,3 +7,7 @@ class PipelineFactory:
     @staticmethod
     def _create_example_gen() -> CustomExampleGen:
         return CustomExampleGen()
+
+    @staticmethod
+    def _create_statistics_gen(example_gen: CustomExampleGen) -> StatisticsGen:
+        return StatisticsGen(examples=example_gen.outputs["examples"])
