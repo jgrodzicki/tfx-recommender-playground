654b352af93525378bead7ce34232560c4efa6fb
Jakub  Grodzicki <jakub.grodzicki@forecast.global>
2023-01-30 16:31:25 +0100

    Introduce function creating full pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,13 @@
+from typing import List
+
+from tfx.components.base.base_node import BaseNode
 from tfx.components.pusher.component import Pusher
 from tfx.components.schema_gen.component import SchemaGen
 from tfx.components.statistics_gen.component import StatisticsGen
 from tfx.components.trainer.component import Trainer
 from tfx.components.transform.component import Transform
+from tfx.orchestration.metadata import ConnectionConfigType
+from tfx.orchestration.pipeline import Pipeline
 from tfx.v1.proto import EvalArgs, PushDestination, TrainArgs
 
 from src.components.consts import EPOCHS_CONFIG_FIELD_NAME
@@ -60,4 +65,37 @@ class PipelineFactory:
             model=trainer.outputs["model"],
             model_blessing=evaluator.outputs["blessing"],
             push_destination=PushDestination(filesystem=PushDestination.Filesystem(base_directory=serving_model_dir)),
+        )
+
+    @staticmethod
+    def create(
+        pipeline_name: str,
+        pipeline_root: str,
+        metadata_connection_config: ConnectionConfigType,
+        serving_model_dir: str,
+        metric_name: str,
+        metric_threshold: float,
+    ) -> Pipeline:
+        example_gen = PipelineFactory._create_example_gen()
+        statistics_gen = PipelineFactory._create_statistics_gen(example_gen=example_gen)
+        schema_gen = PipelineFactory._create_schema_gen(statistics_gen=statistics_gen)
+        transform = PipelineFactory._create_transform(example_gen=example_gen, schema_gen=schema_gen)
+        trainer = PipelineFactory._create_trainer(transform=transform)
+        evaluator = PipelineFactory._create_evaluator(
+            trainer=trainer,
+            metric_name=metric_name,
+            metric_threshold=metric_threshold,
+        )
+        pusher = PipelineFactory._create_pusher(
+            trainer=trainer,
+            evaluator=evaluator,
+            serving_model_dir=serving_model_dir,
+        )
+
+        components: List[BaseNode] = [example_gen, statistics_gen, schema_gen, transform, trainer, evaluator, pusher]
+        return Pipeline(
+            pipeline_name=pipeline_name,
+            pipeline_root=pipeline_root,
+            metadata_connection_config=metadata_connection_config,
+            components=components,
         )
